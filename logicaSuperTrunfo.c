7b4caf8c861bcacfba33a804646bb20d7b3d436c
Kelwin Cavalheiro <108766793+Kelwincavalheiro@users.noreply.github.com>
2025-10-29 23:52:04 +0000

    Primeiro commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,38 +6,146 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+     char escolhaLuta;
+   
+    char estadoCarta1;
+    int codigoCarta1;
+    char nomeCidadeCarta1[50];
+    unsigned int populacaoCarta1;
+    float areaCarta1;
+    float pibCarta1;
+    int pontosTuristicosCarta1;
 
+    char estadoCarta2;
+    int codigoCarta2;
+    char nomeCidadeCarta2[50];
+    unsigned int populacaoCarta2;
+    float areaCarta2;
+    float pibCarta2;
+    int pontosTuristicosCarta2;
+
+    //carta 1
+    printf("Digite o estado da Carta 1: ");
+    scanf(" %c", &estadoCarta1);  
+    printf("Digite o código da Carta 1: ");
+    scanf("%d", &codigoCarta1); 
+    printf("Digite o nome da cidade da Carta 1: ");
+    scanf(" %49[^\n]", nomeCidadeCarta1);
+    printf("Digite a população da Carta 1: ");
+    scanf("%u", &populacaoCarta1); 
+    printf("Digite a área da Carta 1 (em km²): ");
+    scanf("%f", &areaCarta1); 
+    printf("Digite o PIB da Carta 1: ");
+    scanf("%f", &pibCarta1); 
+    printf("Digite o número de pontos turísticos da Carta 1: ");
+    scanf("%d", &pontosTuristicosCarta1);
+
+
+    printf("\nCarta1:\n");
+    printf("Código: %d\n", codigoCarta1);
+    printf("Nome da Cidade: %s\n", nomeCidadeCarta1);
+    printf("População: %d\n", populacaoCarta1);
+    printf("Área: %f\n", areaCarta1);
+    printf("PIB: %f\n", pibCarta1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicosCarta1);
+
+    //carta2
+    printf("\n Digite o estado da Carta 2: ");
+    scanf(" %c", &estadoCarta2);  
+    printf("Digite o código da Carta 2: ");
+    scanf("%d", &codigoCarta2); 
+    printf("Digite o nome da cidade da Carta 2: ");
+    scanf(" %49[^\n]", nomeCidadeCarta2);
+    printf("Digite a população da Carta 2: ");
+    scanf("%u", &populacaoCarta2); 
+    printf("Digite a área da Carta 2 (em km²): ");
+    scanf("%f", &areaCarta2); 
+    printf("Digite o PIB da Carta 1: ");
+    scanf("%f", &pibCarta2); 
+    printf("Digite o número de pontos turísticos da Carta 2: ");
+    scanf("%d", &pontosTuristicosCarta2);
+
+    printf("\nCarta 2:\n");
+    printf("Código: %d\n", codigoCarta2);
+    printf("Nome da Cidade: %s\n", nomeCidadeCarta2);
+    printf("População: %d\n", populacaoCarta2);
+    printf("Área: %f\n", areaCarta2);
+    printf("PIB: %f\n", pibCarta2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicosCarta2);
+
+    printf("\nEscolha o atributo da luta, sendo que\n");
+    printf("população: 1\n area: 2\n pib: 3\n pontos turisticos: 4\n todos os atributos: 5\n");
+    scanf(" %c", &escolhaLuta);
+
+    if(escolhaLuta == '1'){
+        if(populacaoCarta1 > populacaoCarta2){
+            printf("atributo população: Carta 1 venceu\n");
+        }else if(populacaoCarta1 == populacaoCarta2){
+            printf("atributo população: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo população: Carta 2 venceu\n");
+        }
+    }
+     if(escolhaLuta == '2'){
+        if(areaCarta1 > areaCarta2){
+            printf("atributo area: Carta 1 venceu\n");           
+        }else if(areaCarta1 == areaCarta2){
+            printf("atributo area: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo area: Carta 2 venceu\n");
+        }
+    }
+     if(escolhaLuta == '3'){
+        if(pibCarta1 > pibCarta2){
+            printf("atributo pib: Carta 1 venceu\n");   
+        }else if(pibCarta1 == pibCarta2){
+            printf("atributo pib: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo pib: Carta 2 venceu\n");
+        }
+    }
+     if(escolhaLuta == '4'){
+        if(pontosTuristicosCarta1 > pontosTuristicosCarta2){
+            printf("pontos turisticos: Carta 1 venceu\n");           
+        }else if(pontosTuristicosCarta1 == pontosTuristicosCarta2){
+            printf("pontos turisticos: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("pontos turisticos: Carta 2 venceu\n");
+        }
+    }
+     if(escolhaLuta == '5'){
+         if(populacaoCarta1 > populacaoCarta2){
+            printf("atributo população: Carta 1 venceu\n");
+        }else if(populacaoCarta1 == populacaoCarta2){
+            printf("atributo população: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo população: Carta 2 venceu\n");
+        }
+     
+        if(areaCarta1 > areaCarta2){
+            printf("atributo area: Carta 1 venceu\n");           
+        }else if(areaCarta1 == areaCarta2){
+            printf("atributo area: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo area: Carta 2 venceu\n");
+        }
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+        if(pibCarta1 > pibCarta2){
+            printf("atributo pib: Carta 1 venceu\n");   
+        }else if(pibCarta1 == pibCarta2){
+            printf("atributo pib: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("atributo pib: Carta 2 venceu\n");
+        }
+    
+        if(pontosTuristicosCarta1 > pontosTuristicosCarta2){
+            printf("pontos turisticos: Carta 1 venceu\n");           
+        }else if(pontosTuristicosCarta1 == pontosTuristicosCarta2){
+            printf("pontos turisticos: Carta 1 e carta 2 empataram\n");           
+        }else{
+            printf("pontos turisticos: Carta 2 venceu\n");
+        }
+     }
 
     return 0;
 }
